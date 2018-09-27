ff9ce89fe29465a73aee843c29ae44770fcbb61d
Andrew <drewzisa@gmail.com>
2018-09-26 22:05:00 -0700

    changed config again!

--- src/config/connections.js
+++ src/config/connections.js
@@ -1,4 +1,4 @@
 export default {
-    baseURI: "https://jvr0g2usc1.execute-api.us-west-2.amazonaws.com/Prod"
+    baseURI: "https://3mmv8v4rb4.execute-api.us-west-2.amazonaws.com/Prod"
     //  baseURI: "http://localhost:4967"
 }

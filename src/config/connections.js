c9eef02569817e2eb9d824c5f903bc16bb8f3c85
Andrew <drewzisa@gmail.com>
2018-09-26 21:52:41 -0700

    Changed apigateway url

--- src/config/connections.js
+++ src/config/connections.js
@@ -1,4 +1,4 @@
 export default {
-    baseURI: "https://c622s3t4i4.execute-api.us-west-2.amazonaws.com/Prod"
+    baseURI: "https://jvr0g2usc1.execute-api.us-west-2.amazonaws.com/Prod"
     //  baseURI: "http://localhost:4967"
 }
